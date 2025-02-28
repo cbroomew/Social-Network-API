ec3c5d6c34cf714526d32506b46767cfb9f202ea
cbroomew <chad.webster@yahoo.com>
2025-02-28 18:43:06 -0500

    structured files and set up server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const db = require('./config/connect');
+const routes = require('./routes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`API is live on http://localhost:${PORT}`));
+});
